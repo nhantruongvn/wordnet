721e4f5901ba9e033afc194bf9141fee65cacc5b
nhantruongvn <108405814+nhantruongvn@users.noreply.github.com>
2023-02-17 01:40:49 +0700

    refactor code

--- src/main/java/io/syntagmalab/services/wordnet/wordlist5000/GenerateWordlist.java
+++ src/main/java/io/syntagmalab/services/wordnet/wordlist5000/GenerateWordlist.java
@@ -16,46 +16,42 @@ public class GenerateWordlist {
         final String INPUT_FILE = "./src/main/resources/static/5000_words_with_POS.txt";
         final String OUTPUT_FILE = "./src/main/resources/static/5000_words_with_definitions.txt";
 
-        try {
-            // create output file
-            File outputFile = new File(OUTPUT_FILE);
-            FileOutputStream fos = new FileOutputStream(outputFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos));
+        // create output file
+        File outputFile = new File(OUTPUT_FILE);
+        FileOutputStream fos = new FileOutputStream(outputFile);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos));
 
-            // read input file
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE));
-            String line = bufferedReader.readLine();
-            String word;
-            Integer pos;
+        // read input file
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE));
+        String line = bufferedReader.readLine();
+        String word;
+        Integer pos;
 
-            while (null != line) {
-                // split each line into word and POS-number
-                word = line.split("\\_")[0];
-                pos = Integer.parseInt(line.split("\\_")[1]);
+        while (null != line) {
+            // split each line into word and POS-number
+            word = line.split("\\_")[0];
+            pos = Integer.parseInt(line.split("\\_")[1]);
 
-                URL url = new URL("file", null, PATH);
-                IDictionary dict = new Dictionary(url);
-                dict.open();
+            URL url = new URL("file", null, PATH);
+            IDictionary dict = new Dictionary(url);
+            dict.open();
 
-                // get all senses of a word
-                IIndexWord iIndexWord = dict.getIndexWord(word, POS.getPartOfSpeech(pos));
-                if (iIndexWord == null) {
-                    throw new NullPointerException("The word '" + word + "' cannot be found.");
-                }
-                List<IWordID> iWordIdList = iIndexWord.getWordIDs();
-
-                // write keyword-definition pairs in output file
-                for (IWordID iWordId : iWordIdList) {
-                    String keyword = dict.getWord(iWordId).getLemma();
-                    String definition = dict.getWord(iWordId).getSynset().getGloss();
-                    bufferedWriter.write(keyword + "\t" + definition);
-                    bufferedWriter.newLine();
-                }
-                line = bufferedReader.readLine();
+            // get all senses of a word
+            IIndexWord iIndexWord = dict.getIndexWord(word, POS.getPartOfSpeech(pos));
+            if (iIndexWord == null) {
+                throw new NullPointerException("The word '" + word + "' cannot be found.");
             }
-            bufferedReader.close();
-        } catch (IOException ioException) {
-            throw new RuntimeException(ioException);
+            List<IWordID> iWordIdList = iIndexWord.getWordIDs();
+
+            // write keyword-definition pairs in output file
+            for (IWordID iWordId : iWordIdList) {
+                String keyword = dict.getWord(iWordId).getLemma();
+                String definition = dict.getWord(iWordId).getSynset().getGloss();
+                bufferedWriter.write(keyword + "\t" + definition);
+                bufferedWriter.newLine();
+            }
+            line = bufferedReader.readLine();
         }
+        bufferedReader.close();
     }
 }
